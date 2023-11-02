b56f1ac8a2ce9d0eb4a86387377de0d6fffbfd16
Baik.Juwon <duaneqor@naonsoft.com>
2023-11-02 18:36:15 +0900

    [mergeResource]
    - 리소스 merge 기능 완료
    - 완료 결과물 targer 폴더에 삽입기능 추가
    - 추가된 리소스 한 줄로 나오는 결함 해결
    - 그룹웨어 디렉터리 상수화 및 환경설정 주소 중복입력 결함 해결
    - naonDir.yml이 없을 경우 프로그램 실행 불가하도록 변경

--- pages/mainPage.py
+++ pages/mainPage.py
@@ -1,17 +1,19 @@
 import tkinter
+from tkinter import messagebox
 import configparser
+import os, shutil, re
 
 class mainPage():
 
     def __init__(self):
 
         console = tkinter.Tk()
-        self.newWindow = None
+        self.dir = "./다국어"
 
         # UI 설정
         self.initUI(console)
         # 기능 설정
-        self.initFeature(console)
+        self.addFeature(console)
 
         console.mainloop()
     
@@ -36,14 +38,140 @@ class mainPage():
     
         console.config(menu=menubar)
 
-    def initFeature(self, console):
-        print('a')
+    def addFeature(self, console):
+        mergeResources = tkinter.Button(console, text="다국어 리소스 병합", overrelief="solid", width=15, command=self.mergeResources, repeatdelay=1000, repeatinterval=100)
+        mergeResources.pack()
         
+    def mergeResources(self):
+        
+        if not self.checkIniExists() :
+            if (messagebox.askokcancel("알림", "저장된 경로가 올바르지 않습니다.\n환경설정을 진행합니다.")) :
+                from pages import optionPage
+                optionPage.optionPage()
+                return
+            
+        gwDir = self.config["ML_DIR"]["GW_DIR"]
+        iamDir = self.config["ML_DIR"]["IAM_DIR"]
+
+        backUpDir = "./다국어/backUp"
+
+        if os.path.exists(backUpDir):
+            shutil.rmtree(backUpDir)
+
+        os.makedirs(backUpDir + "/gw/js")
+        os.makedirs(backUpDir + "/gw/properties")
+        os.makedirs(backUpDir + "/iam/js")
+        os.makedirs(backUpDir + "/iam/properties")
+        os.makedirs(backUpDir + "/merged/js")
+        os.makedirs(backUpDir + "/merged/properties")
+
+        dirList = self.config.options("JS_PROPERTIES_DIR")
+
+        for directory in dirList :
+            for root, dirs, files in os.walk(self.config["JS_PROPERTIES_DIR"][directory]) :
+                for file in files :
+                    if file.endswith("." + directory.split('_')[1]) :
+                        shutil.copy(os.path.join(root, file), os.path.join(backUpDir + "/" + directory.split('_')[0] + "/" + directory.split('_')[1], file))
+                
+                    if "gw" in directory :
+                        if file.endswith("." + directory.split('_')[1]) :
+                            shutil.copy(os.path.join(root, file), os.path.join(backUpDir + "/merged/" + directory.split('_')[1], file))
+
+        mergeTrgtFld = [backUpDir + "/iam/js", backUpDir + "/iam/properties"]
+        diffDict = {'js':{},'properties':{}}
+
+        for folder in mergeTrgtFld :
+            for root, dirs, files in os.walk(folder) :
+                for file in files :
+                    resDict = {}
+                    
+                    print(file + " 작업중...")
+                    # iam 파일 읽기
+                    with open(folder + "/" + file, "r", encoding="utf-8") as iamFile :
+                        context = iamFile.readlines()
+                        extn = file.split(".")[1]
+                        for line in context:
+                            if not '=' in line :
+                                continue
+                            if extn == "js" :
+                                if not ' ' in line :
+                                    continue
+                                resDict[line.split("=")[0].split(" ")[1]] = re.sub(r'\n$', '', line.split("=")[1])
+                            elif extn == "properties" :
+                                resDict[line.split("=")[0]] = re.sub(r'\n$', '', line.split("=")[1])
+
+                    # gw 파일 읽어서 dict에서 이미 존재하는 key 제거
+                    with open(folder.replace("/iam/", "/gw/") + "/" + file, "r", encoding="utf-8") as gwFile :
+                        context = gwFile.readlines()
+                        extn = file.split(".")[1]
+                        for line in context:
+                            if not '=' in line :
+                                continue
+                            if extn == "js" :
+                                if not ' ' in line :
+                                    continue
+                                if line.split("=")[0].split(" ")[1] in resDict :
+                                    del resDict[line.split("=")[0].split(" ")[1]]
+                            elif extn == "properties" :
+                                if line.split("=")[0] in resDict :
+                                    del resDict[line.split("=")[0]]
+
+                    # 끝부분 개행 제거
+                    context = [line for line in context if line.strip() != '']
+
+                    # target 폴더 백업물 생성
+                    with open(folder.replace("/iam/", "/merged/") + "/" + file, "w", encoding="utf-8") as mergedFile :
+                        mergedFile.writelines(context)
+
+                    # 남은 리소스 붙여넣기
+                    with open(folder.replace("/iam/", "/merged/") + "/" + file, "a", encoding="utf-8") as mergedFile :
+                        if extn == "js" :
+                            for key, value in resDict.items() :
+                                mergedFile.write("var " + key + "=" + value + "\n")
+                                
+                        elif extn == "properties" :
+                            for key, value in resDict.items() :
+                                mergedFile.write(key + "=" + value + "\n")
+
+                    print(file + " 작업완료!")
+
+        self.copyResourcesToTrgtFld(backUpDir)
+
     def getOptionPage(self):
         from pages import optionPage
         optionPage.optionPage()
     
     def checkIniExists(self):
-        config = configparser.ConfigParser()
-        config.read('./config.ini', encoding='utf-8')
-        return config.has_section("ML_DIR")
+        self.config = configparser.ConfigParser()
+        
+        if not os.path.exists(self.dir):
+            os.makedirs(self.dir)
+
+        self.config.read('./다국어/config.ini', encoding='utf-8')
+
+        if self.config.has_option("ML_DIR", "GW_DIR") and self.config.has_option("ML_DIR", "IAM_DIR") :
+            return os.path.exists(self.config["ML_DIR"]["GW_DIR"]) and os.path.exists(self.config["ML_DIR"]["IAM_DIR"])
+        else :
+            return False
+    
+    def copyResourcesToTrgtFld(self, backUpDir):
+        
+        resDir = [
+            backUpDir + "/merged/js",
+            backUpDir + "/merged/properties"
+        ]
+
+        trgtDir = [
+            self.config["TRGT_DIR"]["js"],
+            self.config["TRGT_DIR"]["properties"]
+        ]
+
+        for directory in trgtDir :
+            if os.path.exists(directory):
+                shutil.rmtree(directory)
+            # os.makedirs(directory)
+        
+        for index in range(len(resDir)) :
+            shutil.copytree(resDir[index], trgtDir[index])
+
+        messagebox.showinfo("알림", "완료")
--- pages/optionPage.py
+++ pages/optionPage.py
@@ -1,13 +1,18 @@
 import tkinter
 from tkinter import filedialog
+from tkinter import messagebox
 import configparser
+import os
+import yaml
 
 class optionPage():
 
     def __init__(self):
 
         self.console = tkinter.Toplevel()
+        self.dir = "./다국어"
         self.config = self.onLoadConfig()
+        self.naonDir = self.onLoadNaonDir(self.console)
 
         # UI 설정
         self.initUI(self.console)
@@ -50,7 +55,10 @@ class optionPage():
             iamDir.insert(0, self.config["ML_DIR"]["IAM_DIR"] if self.config.has_option("ML_DIR", "IAM_DIR") else "")
 
     def askDir(self, target):
-        target.insert(0, filedialog.askdirectory())
+        selectedDir = filedialog.askdirectory()
+        if (selectedDir) :
+            target.delete(0, 'end')
+            target.insert(0, selectedDir)
 
     def onSaveConfig(self, gwDir, iamDir, console):
         config = configparser.ConfigParser()
@@ -61,13 +69,42 @@ class optionPage():
         config.set("ML_DIR", "GW_DIR", gwDir)
         config.set("ML_DIR", "IAM_DIR", iamDir)
 
-        with open("../config.ini", "w") as fp:
+        if not config.has_section("JS_PROPERTIES_DIR") :
+            config.add_section("JS_PROPERTIES_DIR")
+            config.set("JS_PROPERTIES_DIR", "GW_JS", gwDir + self.naonDir['GW']['JS'])
+            config.set("JS_PROPERTIES_DIR", "GW_JS_PORTLET", gwDir + self.naonDir['GW']['JS_PORTLET'])
+            config.set("JS_PROPERTIES_DIR", "IAM_JS", iamDir + self.naonDir['IAM']['JS'])
+            config.set("JS_PROPERTIES_DIR", "GW_PROPERTIES", gwDir + self.naonDir['GW']['PROPERTIES'])
+            config.set("JS_PROPERTIES_DIR", "IAM_PROPERTIES", iamDir + self.naonDir['IAM']['PROPERTIES'])
+        
+        if not config.has_section("TRGT_DIR") :
+            config.add_section("TRGT_DIR")
+            config.set("TRGT_DIR", "JS", gwDir + "/naon-module-web/target/naon-module-web/resources/biz/i18n/js")
+            config.set("TRGT_DIR", "PROPERTIES", gwDir + "/naon-module-web/target/naon-module-web/resources/biz/i18n/properties")
+
+        with open("./다국어/config.ini", "w") as fp:
             config.write(fp)
         
-        if (tkinter.messagebox.askokcancel("알림", "저장 완료되었습니다.")) :
+        if (messagebox.askokcancel("알림", "저장 완료되었습니다.")) :
             console.destroy()
 
     def onLoadConfig(self):
         config = configparser.ConfigParser()
-        config.read('./config.ini', encoding='utf-8')
+
+        if not os.path.exists(self.dir):
+            os.makedirs(self.dir)
+        
+        config.read('./다국어/config.ini', encoding='utf-8')
         return config
+    
+    def onLoadNaonDir(self, console):
+        path = 'naonDir.yml'
+
+        try :
+            with open(path, 'r') as file:
+                dirData = yaml.safe_load(file)
+                return dirData
+            
+        except FileNotFoundError :
+            if (messagebox.showerror("알림", "naonDir이 없습니다.\n헬국어 프로젝트 위키페이지에서 yml을 다운로드 받아,\n실행파일과 같은 폴더에 넣어주세요.")) :
+                console.destroy()
